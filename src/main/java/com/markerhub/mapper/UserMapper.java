9d8c85a70a46935931acf176bc0a61fbaf6da257
KID-1412-BOY <1441489665@qq.com>
2022-03-03 23:49:04 +0800

    Initial commit

---
+++ src/main/java/com/markerhub/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.markerhub.mapper;
+
+import com.markerhub.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
